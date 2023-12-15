ac1da34ab1e994c00c5d66a6832136a6729cd921
cedrata <greggius9891@gmail.com>
2023-12-15 15:13:50 +0100

    fix: comment

--- pkg/rest/constants.go
+++ pkg/rest/constants.go
@@ -2,7 +2,6 @@ package rest
 
 const (
 	JSONContentType = "application/json"
-	// Host contains protocol and hostname
     TemplateUrlGetProjIssues = "{{.Protocol}}://{{.Host}}/rest/api/2/search?jql=project={{.ProjectId}}+order+by+duedate&fields=id,key"
 )
 
