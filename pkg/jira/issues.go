b2ab99eb5f7234cb200bf0585b3376d3bd0997bc
cedrata <greggius9891@gmail.com>
2024-01-16 11:22:25 +0100

    feat: to string Issue + Issues

--- pkg/jira/issues.go
+++ pkg/jira/issues.go
@@ -1,8 +1,6 @@
 package jira
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Issue struct {
 	Key         string
@@ -12,13 +10,25 @@ type Issue struct {
 	Summary     string
 }
 
+type Issues []Issue
+
 func (i Issue) String() string {
 	return fmt.Sprintf(
-		"\nkey: %s\nsummary:%s\nassignee: %s\nstatus: %s\ndescription: %s\n",
+		"key: %s\nsummary: %s\nassignee: %s\nstatus: %s\ndescription: %s",
 		i.Key,
 		i.Summary,
 		i.Assignee,
 		i.Status,
 		i.Description,
 	)
+}
+
+func (i Issues) String() string {
+    var res string
+
+    for k := range i {
+        res = fmt.Sprintf("%s\n\n%s", res, i[k].String())
+    }
+
+    return res[2:]
 }
