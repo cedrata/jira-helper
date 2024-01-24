640ddbf3e8cd2310f558e9bd4ebdf74a9c5b4753
cedrata <greggius9891@gmail.com>
2024-01-24 11:10:15 +0100

    style: remove unused and move to single file type

--- pkg/rest/constants.go
+++ pkg/rest/constants.go
@@ -1,9 +1,11 @@
 package rest
 
+type Operation string
+
 const (
 	JSONContentType = "application/json"
 
-    // Available operations
+	// Available operations
 	GetIssues       Operation = "get-issues"
 	GetTransitions  Operation = "get-transitions"
 	PostTransitions Operation = "post-transitions"
